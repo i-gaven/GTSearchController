bb84ab55e49406683dddf3835d9f0a6890222c59
zhaoguoteng <zhaoguoteng@haodai.com>
2015-07-12 11:04:12 +0800

    first commit

---
+++ GTSearchController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTSearchController
+//
+//  Created by 赵国腾 on 15/7/12.
+//  Copyright (c) 2015年 zhaoguoteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTSearchController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GTSearchController
+//
+//  Created by 赵国腾 on 15/7/12.
+//  Copyright (c) 2015年 zhaoguoteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
